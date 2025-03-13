6a92c48642e748f73dd6b11bf392c3405b97ffe2
Hector Vera <hveramtz.inf@gmail.com>
2025-03-13 00:14:21 -0300

    falta implementar menu hamburguesa

--- parkinspotPWA/src/components/web/queEs/queEs.jsx
+++ parkinspotPWA/src/components/web/queEs/queEs.jsx
@@ -4,6 +4,7 @@ import { useInView } from 'react-intersection-observer'
 import './queEs.css'
 
 export default function QueEs() {
+
   const { ref, inView } = useInView({
     triggerOnce: true, // La animación se activará solo una vez
     threshold: 0.1, // El porcentaje del componente visible para activar la animación
@@ -26,14 +27,16 @@ export default function QueEs() {
         <h2>¿Que es?</h2>
         <p>ResiPark es una PWA (Progres Web App), para poder ser un aporte a la organizacion, el buen convivir y la convivencia entre los residentes, visitas, porteros y servicios de emergencia.</p>
       </motion.div>
-      <motion.div 
+      <div 
         className='ContenedorImagen'
-        initial={{ x: 100, opacity: 0 }}
-        animate={{ x: inView ? 0 : 100, opacity: inView ? 1 : 0 }}
-        transition={{ duration: 1.5 }}
       >
-        <img src='./img/fordfusion.png' alt='Que es ResiPark' />
-      </motion.div>
+        <motion.img className='carontop'
+                initial={{ x: 1000, opacity: 0, rotate: 0 }}
+                animate={{ x: inView ? 0 : 1000, opacity: inView ? 1 : 0, rotate: inView ? 430 : 0 }}
+                transition={{ duration: 2.5 }} 
+                src='./img/carontop.png'/>
+        <motion.img/>
+      </div>
     </motion.div>
   )
 }
